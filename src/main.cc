3255745f5a0115e9ce04822e8d36a8deddb2e1a2
Frank LENORMAND <lenormf@gmail.com>
2019-11-22 12:15:22 +0100

    src: Allow user modes to display a docstring
    Fixes #3099

--- src/main.cc
+++ src/main.cc
@@ -326,7 +326,7 @@ static const EnvVarDesc builtin_env_vars[] = { {
     }, {
         "user_modes", false,
         [](StringView name, const Context& context) -> Vector<String>
-        { return context.keymaps().user_modes(); }
+        { return context.keymaps().user_modes() | transform(&KeymapManager::UserMode::name) | gather<Vector<String>>(); }
     }, {
         "window_range", false,
         [](StringView name, const Context& context) -> Vector<String>
